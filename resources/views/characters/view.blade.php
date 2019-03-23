a8f4a27d92d3969bebdb703b14466788177d368f
vitorpre <@Tordo123>
2019-03-22 21:43:51 -0300

    layouts - Corrigido layout da view de personagem

--- resources/views/characters/view.blade.php
+++ resources/views/characters/view.blade.php
@@ -4,19 +4,19 @@
 
 @section('content')
 
-    <div class="row text-center align-content-center">
+    <div class="text-center align-content-center">
         <img src="{{ $character->thumbnail->path . '.' . $character->thumbnail->extension }}"
              style="max-height: 350px;" class="img-thumbnail align-content-center mb-3" />
 
         <section class="details row mb-3">
-            <h2 class="card-title mb-2">{{ $character->name }}</h2>
-            <p class="card-text">{{ $character->description }}</p>
+            <h2 class="card-title mb-2 col-12">{{ $character->name }}</h2>
+            <p class="card-text col-12" >{{ $character->description }}</p>
         </section>
 
         <section class="comics row mb-3">
-            <h2 class="mb-2">Comics</h2>
+            <h2 class="mb-2 col-12">Comics</h2>
 
-            <ul class="list-group list-group-flush">
+            <ul class="list-group list-group-flush col-12">
                 @foreach($character->comics->items as $comic)
                     <a href="{{ $comic->resourceURI }}"><li class="list-group-item">{{ $comic->name }}</li></a>
                 @endforeach
@@ -25,9 +25,9 @@
         </section>
 
         <section class="series row mb-3">
-            <h2 class="mb-2">Series</h2>
+            <h2 class="mb-2 col-12">Series</h2>
 
-            <ul class="list-group list-group-flush">
+            <ul class="list-group list-group-flush col-12">
                 @foreach($character->series->items as $serie)
                     <a href="{{ $serie->resourceURI }}"><li class="list-group-item">{{ $serie->name }}</li></a>
                 @endforeach
